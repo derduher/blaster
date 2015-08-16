a2cb50c699d00463cfc2cb2f78bbec83ea9ba838
Patrick Weygand <patrick.weygand@ubnt.com>
2015-08-15 18:01:19 -0700

    debug text in canvas fullscreen touch

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,14 @@
 'use strict';
+/*jshint bitwise: false*/
 // Code goes here
-var xmin, xmax, ymin, ymax, canvas, ctx, fpsOut, fps = 0, now,
-  lastPUpdate, lastUpdate, fpsFilter = 600, fpsDrawInterval = 3000;
-lastPUpdate = lastUpdate = (new Date())*1;
+var xmin, xmax, ymin, ymax, canvas, ctx, fpsOut, itemsOut, craft, fps = 0,
+  items = [],
+  lastUpdate, fpsFilter = 60;
+var enterFullScreen, de = document.documentElement;
+lastUpdate = 0;
+
+var debug, debugText = '';
 var pSpeed = 50;
-
-var craft = {
-  x: 0,
-  y: 0,
-  xDir: 0,
-  yDir: 0,
-  path: null,
-  width: 50
-};
-
-var projectiles = [{
-  x: 150,
-  y: 150,
-  width: 2,
-  exists: false
-}];
 
 var ctrl = {
   lp: false,
@@ -30,28 +19,123 @@ var ctrl = {
 };
 
 const a = 65,
-d = 68,
-w = 87,
-s = 83,
-space = 32,
-padding = 3;
+  d = 68,
+  w = 87,
+  s = 83,
+  space = 32,
+  left = 37,
+  right = 39,
+  up = 38,
+  down = 40,
+  padding = 3,
+  esc = 27;
 
 const speed = 0.2;
 var posDir = speed;
 var negDir = speed * -1;
 
-var cfg = {
-  l: a,
-  r: d,
-  u: w,
-  d: s,
-  f: space
-};
-cfg[a] = 'l';
-cfg[d] = 'r';
-cfg[w] = 'u';
-cfg[s] = 'd';
+var cfg = {};
+cfg[a] = cfg[left] = 'l';
+cfg[d] = cfg[right] = 'r';
+cfg[w] = cfg[up] = 'u';
+cfg[s] = cfg[down] = 'd';
 cfg[space] = 'f';
+cfg[esc] = 'toggleFS';
+
+function Projectile (craft) {
+  this.x = craft.x + craft.width / 2;
+  this.y = craft.y - 2;
+  this.width = 3;
+  this.vx = craft.vx;
+  this.vy = -0.5 + craft.vy;
+  this.boundToCanvas = false;
+}
+
+Projectile.prototype.tick = function tick (dt, now) {
+  this.vx *= (1 - dt * 0.0008);
+  this.vy *= (1 - dt * 0.0008);
+};
+
+Projectile.prototype.draw = function draw (ctx) {
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.fillRect(0, 0, this.width, this.width);
+  ctx.restore();
+};
+
+function Roid (cw) {
+  this.width = (9 + 100 * Math.random()) | 0;
+  this.health = this.initialHealth = (0.5 + Math.random()) * this.width * this.width | 0;
+  this.x = cw * Math.random();
+  this.y = 0 - this.width;
+  this.vx = 0.02 * (-0.5 + Math.random());
+  this.vy = 0.1 * Math.random();
+  this.color = 'black';
+  this.path = new Path2D();
+  this.path.arc(this.width / 2, this.width / 2, this.width / 2, 0, Math.PI * 2);
+}
+
+Roid.prototype.tick = function tick (dt, now) {};
+
+Roid.prototype.draw = function draw (ctx) {
+  ctx.save();
+  ctx.fillStyle = 'rgb(' + (148 + 107 * (1 - this.health / this.initialHealth) | 0) + ',0,234)';
+  ctx.translate(this.x, this.y);
+  ctx.fill(this.path);
+  ctx.restore();
+};
+
+function Craft (canvasW, canvasH) {
+  this.lastFire = 0;
+  this.boundToCanvas = true;
+  this.vx = 0;
+  this.vy = 0;
+  this.width = 50;
+  this.path = new Path2D();
+  this.health = 1000;
+
+  this.path.moveTo(0, this.width);
+  this.path.lineTo(this.width, this.width);
+  this.path.lineTo(this.width / 2, 0);
+  this.path.closePath();
+
+  this.x = canvasW / 2 - this.width / 2;
+  this.y = canvasH - this.width - padding;
+}
+
+Craft.prototype.draw = function draw (ctx) {
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.stroke(this.path);
+  ctx.restore();
+};
+
+Craft.prototype.tick = function tick (dt, now) {
+  var pdt = now - this.lastFire;
+
+  // set ctrl dir
+  if (ctrl.l && !ctrl.r) {
+    this.vx = negDir;
+  } else if (ctrl.r && !ctrl.l) {
+    this.vx = posDir;
+  } else {
+    this.vx = 0;
+  }
+
+  // set ctrl dir
+  if (ctrl.d && !ctrl.u) {
+    this.vy = posDir;
+  } else if (ctrl.u && !ctrl.d) {
+    this.vy = negDir;
+  } else {
+    this.vy = 0;
+  }
+
+  if (ctrl.f && pdt > pSpeed) {
+    items.push(new Projectile(this));
+    this.lastFire = now;
+  }
+};
 
 function kd (e) {
   ctrl[cfg[e.keyCode]] = true;
@@ -61,106 +145,170 @@ function ku (e) {
   ctrl[cfg[e.keyCode]] = false;
 }
 
-function draw () {
-  var frameTDelta, pTdelta;
-  now = new Date();
-  frameTDelta = now - lastUpdate;
-  pTdelta     = now - lastPUpdate;
-  if (frameTDelta){
-    var thisFrameFPS = 1000 / frameTDelta;
-    fps += (thisFrameFPS - fps) / fpsFilter;
+function tm (e) {
+  var t = e.changedTouches[0];
+  ctrl.l = (craft.x - t.pageX) > 5;
+  ctrl.r = (craft.x - t.pageX) < -5;
+  ctrl.u = (craft.y - t.pageY) > 5;
+  ctrl.d = (craft.y - t.pageY) < -5;
+  ctrl.f = true;
+  canvas[enterFullScreen]();
+  e.preventDefault();
+}
 
-    lastUpdate = now;
-  }
+function te (e) {
+  ctrl.l = false;
+  ctrl.r = false;
+  ctrl.u = false;
+  ctrl.d = false;
+  ctrl.f = false;
+  e.preventDefault();
+}
 
-  // set ctrl dir
-  if (ctrl.l && !ctrl.r) {
-    craft.xDir = negDir;
-  } else if (ctrl.r && !ctrl.l) {
-    craft.xDir = posDir;
+function intersect (a, b) {
+  return a.x > b.x && a.x < b.x + b.width && a.y > b.y && a.y < b.y + b.width;
+}
+
+function updateCanvasBoundaries () {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+  xmax = canvas.width - padding;
+  xmin = padding;
+  ymin = padding;
+  ymax = canvas.height - padding;
+}
+if (de.requestFullscreen) {
+  enterFullScreen = 'requestFullscreen';
+} else if (de.msRequestFullscreen) {
+  enterFullScreen = 'msRequestFullscreen';
+} else if (de.mozRequestFullScreen) {
+  enterFullScreen = 'mozRequestFullScreen';
+} else if (de.webkitRequestFullscreen) {
+  enterFullScreen = 'webkitRequestFullscreen';
+}
+
+function toggleFullScreen (el) {
+  if (el && !document.fullscreenElement && // alternative standard method
+    !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) { // current working methods
+    el[enterFullScreen]();
   } else {
-    craft.xDir = 0;
-  }
-
-  // set ctrl dir
-  if (ctrl.d && !ctrl.u) {
-    craft.yDir = posDir;
-  } else if (ctrl.u && !ctrl.d) {
-    craft.yDir = negDir;
-  } else {
-    craft.yDir = 0;
-  }
-
-  if (ctrl.f && pTdelta > pSpeed) {
-    projectiles.push({x: craft.x + craft.width/2, y: craft.y - 2});
-    lastPUpdate = now;
-  }
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.save();
-  ctx.translate(craft.x, craft.y);
-  ctx.stroke(craft.path);
-  ctx.restore();
-
-  projectiles.forEach(function (p, idx, ps) {
-    if (p.y > ymin) {
-      p.y -= frameTDelta * 0.5;
-      ctx.save();
-      ctx.translate(p.x, p.y);
-      ctx.fillRect(0, 0, 3, 3);
-      ctx.restore();
-    } else {
-      ps.splice(idx, 1);
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
     }
-  });
-    //console.log(projectile.x |0, projectile.y |0, craft.x |0, craft.y |0);
-
-  // move the craft
-  if ((craft.xDir !== posDir || craft.x < xmax) && (craft.xDir !== negDir || craft.x > xmin)) {
-    craft.x += craft.xDir * frameTDelta;
   }
-  if ((craft.yDir !== posDir || craft.y < ymax) && (craft.yDir !== negDir || craft.y > ymin)) {
-    craft.y += craft.yDir * frameTDelta;
-  }
-  window.requestAnimationFrame(draw);
+  updateCanvasBoundaries();
 }
 
 function drawFPS () {
-  fpsOut.innerHTML = fps.toFixed(1);
-  console.log(projectiles.length);
+  ctx.fillText(fps.toFixed(1), 0, 18);
+  ctx.fillText(items.length, 0, 36);
+  ctx.fillText(debugText, 0, 54);
+}
+
+function draw (tFrame) {
+  var dt;
+  dt = tFrame - lastUpdate;
+  if (dt) {
+    var thisFrameFPS = 1000 / dt;
+    fps += (thisFrameFPS - fps) / fpsFilter;
+
+    lastUpdate = tFrame;
+  }
+  if (ctrl.toggleFS) {
+    toggleFullScreen();
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFPS();
+
+  if (Math.random() > 0.99) {
+    items.push(new Roid(canvas.width));
+  }
+
+  var cullQ = [];
+  //console.log(items.length);
+  items.forEach(function(i, idx, items) {
+    i.tick(dt, tFrame);
+    var x = i.x + i.vx * dt,
+      y = i.y + i.vy * dt;
+
+    if (((i.vx <= 0 || x < xmax - i.width) && (i.vx >= 0 || x > xmin)) ||
+      !i.boundToCanvas && x < xmax && x > -100
+    ) {
+      i.x = x;
+    } else if (!i.boundToCanvas) {
+      cullQ.push(idx);
+    }
+    if (((i.vy <= 0 || y < ymax - i.width) && (i.vy >= 0 || y > ymin)) ||
+      !i.boundToCanvas && y < ymax && y > -100
+    ) {
+      i.y = y;
+    } else if (!i.boundToCanvas) {
+      cullQ.push(idx);
+    }
+
+    if (i instanceof Projectile) {
+      items.forEach(function (o, odx) {
+        if (!(o instanceof Projectile) && intersect(i, o)) {
+          if (o.boundToCanvas) {
+            debugText = 'craftHit';
+          }
+          o.health -= 200;
+          debugText = o.health;
+          cullQ.push(idx);
+          if (o.health < 9 && cullQ.indexOf(odx) === -1) {
+            o.color = 'orange';
+            cullQ.push(odx);
+          }
+        }
+      });
+    }
+
+    i.draw(ctx);
+  });
+
+  var culled = 0;
+  cullQ.sort(function (a, b) {
+    if (a < b) {
+      return -1;
+    }
+    return a > b ? 1 : 0;
+  });
+  cullQ.forEach(function (v) {
+    items.splice(v - culled++, 1);
+  });
+
+  window.requestAnimationFrame(draw);
 }
 
 function init () {
-
   canvas = document.getElementById('tutorial');
+
+  canvas.onclick = function () {
+    toggleFullScreen(canvas);
+  };
+  updateCanvasBoundaries();
   document.onkeyup = ku;
   document.onkeydown = kd;
+  document.ontouchmove = tm;
+  document.ontouchend = te;
 
-  xmax = canvas.width - craft.width - padding;
-  xmin = padding;
-  ymin = padding;
-  ymax = canvas.height - craft.width - padding;
+  craft = new Craft(canvas.width, canvas.height);
+  items.push(craft);
 
-  craft.x = canvas.width / 2 - craft.width / 2;
-  craft.y = canvas.height - craft.width - padding;
-  craft.xDir = 0;
-  craft.yDir = 0;
   ctx = canvas.getContext('2d');
-
-  craft.path = new Path2D();
-  craft.path.moveTo(0, craft.width);
-  craft.path.lineTo(craft.width, craft.width);
-  craft.path.lineTo(craft.width/2, 0);
-  craft.path.closePath();
-
-  //projectile.path = new Path2D();
-  //projectile.path.moveTo(0, projectile.width);
-  //projectile.path.lineTo(projectile.width, projectile.width);
-  //projectile.path.closePath();
+  ctx.font = '16px serif';
 
   window.requestAnimationFrame(draw);
 
   fpsOut = document.getElementById('fps');
-  setInterval(drawFPS, fpsDrawInterval); 
+  itemsOut = document.getElementById('items');
+  debug = document.getElementById('debug');
 }
 document.body.onload = init;
