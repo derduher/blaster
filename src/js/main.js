c00c83da7344cf3881327f88d91617a8b71b5e51
Patrick Weygand <patrick.weygand@ubnt.com>
2015-08-21 18:15:55 -0700

    working hit detection

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,35 @@
 'use strict';
 /*jshint bitwise: false*/
 // Code goes here
-var xmin, xmax, ymin, ymax, canvas, ctx, fpsOut, itemsOut, craft, fps = 0,
-  items = [],
-  lastUpdate, fpsFilter = 60;
-var enterFullScreen, de = document.documentElement;
-lastUpdate = 0;
+var canvas,
+    hitboxes = [],
+    ctx,
+    enterFullScreen,
+    de = document.documentElement,
+    fpsFilter = 10,
+    Game = {
+      debug: {
+        fps: 0,
+        text: 0,
+        numUpdates: 0,
+        itemL: 0,
+        drawRate: 250
+      },
+      lastRender: window.performance.now(),
+      tickLength: 16.25
+    },
+    Stage = {items: [], spatialManager: null},
+    hatches = new Path2D();
 
-var debug, debugText = '';
+Game.lastTick = Game.lastRender;
+Game.debug.lastRender = Game.lastRender;
+Game.Stage = Stage;
+
+Game.debug.text = 0;
 var pSpeed = 50;
+var fullScreenElementProp;
 
-var ctrl = {
-  lp: false,
-  rp: false,
-  up: false,
-  dp: false,
-  sp: false
-};
+var ctrl = {};
 
 const a = 65,
   d = 68,
@@ -30,7 +43,7 @@ const a = 65,
   padding = 3,
   esc = 27;
 
-const speed = 0.2;
+const speed = 5;
 var posDir = speed;
 var negDir = speed * -1;
 
@@ -42,18 +55,99 @@ cfg[s] = cfg[down] = 'd';
 cfg[space] = 'f';
 cfg[esc] = 'toggleFS';
 
+// https://conkerjo.wordpress.com/2009/06/13/spatial-hashing-implementation-for-fast-2d-collisions/
+class SpatialManager {
+  constructor (scenewidth, sceneheight, cellsize) {
+    this.SceneWidth = scenewidth;
+    this.SceneHeight = sceneheight;
+    this.cf = 1/cellsize;
+
+    this.cols = Math.ceil(scenewidth*this.cf);
+    this.rows = Math.ceil(sceneheight*this.cf);
+    this.buckets = new Map();
+    this.numbuckets = this.rows * this.cols;
+    this.reverse = {};
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        this.reverse[x + y*this.cols |0] = {x:x*cellsize, y:y*cellsize};
+      }
+    }
+    console.log(this.reverse);
+
+    this.clearBuckets();
+  }
+  clearBuckets () {
+    this.buckets.clear();
+    var i;
+    for (i = 0; i < this.numbuckets; i++) {
+      this.buckets.set(i, new Set());
+    }
+  }
+  registerObject (obj) {
+    var cells = this.getIdForObject(obj);
+    var cell, cellId;
+    for (cellId of cells) {
+      cell = this.buckets.get(cellId);
+      if (cell) {
+        cell.add(obj);
+      }
+    }
+  }
+  getIdForObject (obj) {
+    var bucketsObjIsIn = new Set();
+
+    var max = {
+      x: obj.x + obj.width,
+      y: obj.y + obj.width
+    };
+
+    //TopLeft
+    this.addBucket(obj.x, obj.y, this.cf, this.cols, bucketsObjIsIn);
+    //TopRight
+    this.addBucket(max.x, obj.y, this.cf, this.cols, bucketsObjIsIn);
+    //BottomRight
+    this.addBucket(max.x, max.y, this.cf, this.cols, bucketsObjIsIn);
+    //BottomLeft
+    this.addBucket(obj.x, max.y, this.cf, this.cols, bucketsObjIsIn);
+
+    return bucketsObjIsIn;
+  }
+  addBucket (x, y, cf, cols, set) {
+    // ignore collisions offscreen
+    var id = (x*cf | 0) + (y*cf |0)*cols |0;
+    if (id >= 0 && id < this.numbuckets) {
+      set.add(id);
+    }
+  }
+  getNearby (obj) {
+    var id;
+    var nearby = new Set();
+    var ids = this.getIdForObject(obj);
+
+    for (id of ids) {
+      let bucket = this.buckets.get(id);
+      for (let oobj of bucket) {
+        nearby.add(oobj);
+      }
+    }
+    return nearby;
+  }
+}
+
 function Projectile (craft) {
   this.x = craft.x + craft.width / 2;
-  this.y = craft.y - 2;
   this.width = 3;
+  this.health = 150;
   this.vx = craft.vx;
-  this.vy = -0.5 + craft.vy;
+  this.vy = -10 + craft.vy;
+  this.y = craft.y + this.vy;
   this.boundToCanvas = false;
 }
 
-Projectile.prototype.tick = function tick (dt, now) {
-  this.vx *= (1 - dt * 0.0008);
-  this.vy *= (1 - dt * 0.0008);
+Projectile.prototype.tick = function tick () {
+  this.vx *= 0.996;
+  this.vy *= 0.996;
 };
 
 Projectile.prototype.draw = function draw (ctx) {
@@ -68,18 +162,19 @@ function Roid (cw) {
   this.health = this.initialHealth = (0.5 + Math.random()) * this.width * this.width | 0;
   this.x = cw * Math.random();
   this.y = 0 - this.width;
-  this.vx = 0.02 * (-0.5 + Math.random());
-  this.vy = 0.1 * Math.random();
+  this.vx = 0.2 * (-0.5 + Math.random());
+  this.vy = 1 * Math.random();
   this.color = 'black';
   this.path = new Path2D();
   this.path.arc(this.width / 2, this.width / 2, this.width / 2, 0, Math.PI * 2);
 }
 
-Roid.prototype.tick = function tick (dt, now) {};
+Roid.prototype.tick = function tick () {};
 
 Roid.prototype.draw = function draw (ctx) {
   ctx.save();
   ctx.fillStyle = 'rgb(' + (148 + 107 * (1 - this.health / this.initialHealth) | 0) + ',0,234)';
+  //ctx.fillStyle = this.color;
   ctx.translate(this.x, this.y);
   ctx.fill(this.path);
   ctx.restore();
@@ -110,7 +205,7 @@ Craft.prototype.draw = function draw (ctx) {
   ctx.restore();
 };
 
-Craft.prototype.tick = function tick (dt, now) {
+Craft.prototype.tick = function tick (now) {
   var pdt = now - this.lastFire;
 
   // set ctrl dir
@@ -132,7 +227,9 @@ Craft.prototype.tick = function tick (dt, now) {
   }
 
   if (ctrl.f && pdt > pSpeed) {
-    items.push(new Projectile(this));
+    let p = new Projectile(this);
+    Stage.items.push(p);
+    Stage.spatialManager.registerObject(p);
     this.lastFire = now;
   }
 };
@@ -145,15 +242,23 @@ function ku (e) {
   ctrl[cfg[e.keyCode]] = false;
 }
 
+function ts (e) {
+  if (document[fullScreenElementProp]) {
+    e.preventDefault();
+  }
+}
+
 function tm (e) {
-  var t = e.changedTouches[0];
-  ctrl.l = (craft.x - t.pageX) > 5;
-  ctrl.r = (craft.x - t.pageX) < -5;
-  ctrl.u = (craft.y - t.pageY) > 5;
-  ctrl.d = (craft.y - t.pageY) < -5;
-  ctrl.f = true;
-  canvas[enterFullScreen]();
-  e.preventDefault();
+  if (document[fullScreenElementProp]) {
+    var t = e.changedTouches[0];
+    var w2 = Game.craft.width / 2;
+    ctrl.l = (Game.craft.x + w2 - t.pageX) > 5;
+    ctrl.r = (Game.craft.x + w2 - t.pageX) < -5;
+    ctrl.u = (Game.craft.y + Game.craft.width + w2 - t.pageY) > 5;
+    ctrl.d = (Game.craft.y + Game.craft.width + w2 - t.pageY) < -5;
+    ctrl.f = true;
+    e.preventDefault();
+  }
 }
 
 function te (e) {
@@ -162,7 +267,9 @@ function te (e) {
   ctrl.u = false;
   ctrl.d = false;
   ctrl.f = false;
-  e.preventDefault();
+  if (document[fullScreenElementProp]) {
+    e.preventDefault();
+  }
 }
 
 function intersect (a, b) {
@@ -170,12 +277,34 @@ function intersect (a, b) {
 }
 
 function updateCanvasBoundaries () {
-  canvas.width = document.documentElement.clientWidth;
-  canvas.height = document.documentElement.clientHeight;
-  xmax = canvas.width - padding;
-  xmin = padding;
-  ymin = padding;
-  ymax = canvas.height - padding;
+  if (document[fullScreenElementProp]) {
+    canvas.width = window.screen.width;
+    canvas.height = window.screen.height;
+  } else {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+  }
+  Stage.xmax = canvas.width - padding;
+  Stage.xmin = padding;
+  Stage.ymin = padding;
+  Stage.ymax = canvas.height - padding;
+
+  hatches = new Path2D();
+  let w = 109;
+  let max = canvas.width / w;
+  if (canvas.height /w > max) {
+    max = canvas.height/w;
+  }
+  for (var i = 1; i < max; i++) {
+    hatches.moveTo(i * w, 0);
+    hatches.lineTo(i * w, canvas.height);
+    hatches.moveTo(0, i*w);
+    hatches.lineTo(canvas.width, i*w);
+  }
+  hatches.closePath();
+  Game.debug.text++;
+  Stage.spatialManager = new SpatialManager(canvas.width, canvas.height, 109);
+  Stage.items.forEach(Stage.spatialManager.registerObject, Stage.spatialManager);
 }
 if (de.requestFullscreen) {
   enterFullScreen = 'requestFullscreen';
@@ -186,10 +315,19 @@ if (de.requestFullscreen) {
 } else if (de.webkitRequestFullscreen) {
   enterFullScreen = 'webkitRequestFullscreen';
 }
+if (document.fullscreenElement !== undefined) {
+  fullScreenElementProp = 'fullscreenElement';
+} else if (document.mozFullScreenElement !== undefined) {
+  fullScreenElementProp = 'mozFullScreenElement';
+} else if (document.webkitFullscreenElement !== undefined) {
+  fullScreenElementProp = 'webkitFullscreenElement';
+} else if (document.msFullscreenElement !== undefined) {
+  fullScreenElementProp = 'msFullscreenElement';
+}
+
 
 function toggleFullScreen (el) {
-  if (el && !document.fullscreenElement && // alternative standard method
-    !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) { // current working methods
+  if (el && !document[fullScreenElementProp]) { // current working methods
     el[enterFullScreen]();
   } else {
     if (document.exitFullscreen) {
@@ -202,75 +340,88 @@ function toggleFullScreen (el) {
       document.webkitExitFullscreen();
     }
   }
-  updateCanvasBoundaries();
 }
 
-function drawFPS () {
-  ctx.fillText(fps.toFixed(1), 0, 18);
-  ctx.fillText(items.length, 0, 36);
-  ctx.fillText(debugText, 0, 54);
-}
-
-function draw (tFrame) {
-  var dt;
-  dt = tFrame - lastUpdate;
-  if (dt) {
-    var thisFrameFPS = 1000 / dt;
-    fps += (thisFrameFPS - fps) / fpsFilter;
-
-    lastUpdate = tFrame;
-  }
-  if (ctrl.toggleFS) {
-    toggleFullScreen();
-  }
-
+function draw (tFrame, dt) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawFPS();
+  var fs = 70;
+  Stage.items.forEach(function(i, idx, items) {
+    i.draw(ctx);
+  });
 
-  if (Math.random() > 0.99) {
-    items.push(new Roid(canvas.width));
-  }
+  ctx.font = '64px roboto';
+  ctx.fillText(Game.debug.fps.toFixed(1), 0, fs);
+  ctx.fillText(Game.debug.itemL, 0, fs * 2);
+  ctx.fillText(Game.debug.numUpdates, 0, fs * 3);
+  ctx.fillText(Game.debug.text, canvas.width / 2, canvas.height /2);
 
+  ctx.stroke(hatches);
+  ctx.save();
+  ctx.strokeStyle = 'green';
+  ctx.lineWidth = 3;
+  hitboxes.forEach(function (xy) {
+    ctx.strokeRect(xy.x, xy.y, 109, 109);
+  });
+  ctx.restore();
+}
+
+function update (tickTime) {
   var cullQ = [];
-  //console.log(items.length);
-  items.forEach(function(i, idx, items) {
-    i.tick(dt, tFrame);
-    var x = i.x + i.vx * dt,
-      y = i.y + i.vy * dt;
+  if (Math.random() > 0.9) {
+    Stage.items.push(new Roid(canvas.width));
+  }
+  Stage.spatialManager.clearBuckets();
+  Stage.items.forEach(function(i, idx, items) {
+    i.tick(tickTime);
+    var x = i.x + i.vx,
+      y = i.y + i.vy;
 
-    if (((i.vx <= 0 || x < xmax - i.width) && (i.vx >= 0 || x > xmin)) ||
-      !i.boundToCanvas && x < xmax && x > -100
+    if (((i.vx <= 0 || x < Stage.xmax - i.width) && (i.vx >= 0 || x > Stage.xmin)) ||
+      !i.boundToCanvas && x < Stage.xmax && x > -100
     ) {
       i.x = x;
     } else if (!i.boundToCanvas) {
       cullQ.push(idx);
     }
-    if (((i.vy <= 0 || y < ymax - i.width) && (i.vy >= 0 || y > ymin)) ||
-      !i.boundToCanvas && y < ymax && y > -100
+    if (((i.vy <= 0 || y < Stage.ymax - i.width) && (i.vy >= 0 || y > Stage.ymin)) ||
+      !i.boundToCanvas && y < Stage.ymax && y > -100
     ) {
       i.y = y;
     } else if (!i.boundToCanvas) {
       cullQ.push(idx);
     }
+    if (cullQ[cullQ.length - 1] !== idx) {
+      Stage.spatialManager.registerObject(i);
+    }
 
-    if (i instanceof Projectile) {
-      items.forEach(function (o, odx) {
-        if (!(o instanceof Projectile) && intersect(i, o)) {
+  });
+
+  hitboxes = [];
+  Stage.items.forEach(function(i, idx, items) {
+    //if (i instanceof Projectile) {
+      Stage.spatialManager.getNearby(i).forEach(function (o) {
+        //if (!(o instanceof Projectile)) {
+          let ids = Stage.spatialManager.getIdForObject(o);
+          for (let id of ids) {
+            hitboxes.push(Stage.spatialManager.reverse[id]);
+          }
+        //}
+        //if (!(o instanceof Projectile) && intersect(i, o)) {
+        if (intersect(i, o)) {
           if (o.boundToCanvas) {
-            debugText = 'craftHit';
+            Game.debug.text = 'craftHit';
           }
           o.health -= 200;
-          debugText = o.health;
-          cullQ.push(idx);
-          if (o.health < 9 && cullQ.indexOf(odx) === -1) {
-            o.color = 'orange';
+          i.health -= 200;
+          let odx;
+          if (o.health < 9 && cullQ.indexOf((odx = Stage.items.indexOf(o))) === -1) {
             cullQ.push(odx);
+          } else if (i.health < 9 && cullQ.indexOf(idx) === -1) {
+            cullQ.push(idx);
           }
         }
       });
-    }
-
-    i.draw(ctx);
+    //}
   });
 
   var culled = 0;
@@ -281,10 +432,56 @@ function draw (tFrame) {
     return a > b ? 1 : 0;
   });
   cullQ.forEach(function (v) {
-    items.splice(v - culled++, 1);
+    Stage.items.splice(v - culled++, 1);
   });
+}
 
-  window.requestAnimationFrame(draw);
+function updates (numTicks) {
+  var i;
+  for (i=0; i < numTicks; i++) {
+    Game.lastTick += Game.tickLength;
+    update(Game.lastTick);
+  }
+}
+
+function main (tFrame) {
+  var dt,
+      timeSinceTick,
+      thisFrameFPS,
+      nextTick = Game.lastTick + Game.tickLength,
+      numTicks = 0;
+
+  Game.stopMain = window.requestAnimationFrame(main);
+
+  dt = tFrame - Game.lastRender;
+  var ud = tFrame - Game.debug.lastRender > Game.debug.drawRate;
+
+  if (ud) {
+    thisFrameFPS = 1000 / dt;
+    Game.debug.fps += (thisFrameFPS - Game.debug.fps) / fpsFilter;
+    Game.debug.lastRender = tFrame;
+    Game.debug.itemL = Stage.items.length;
+  }
+
+  if (ctrl.toggleFS) {
+    toggleFullScreen();
+  }
+
+  //If tFrame < nextTick then 0 ticks need to be updated (0 is default for numTicks).
+  //If tFrame = nextTick then 1 tick needs to be updated (and so forth).
+  //Note: As we mention in summary, you should keep track of how large numTicks is.
+  //If it is large, then either your game was asleep, or the machine cannot keep up.
+  if (tFrame > nextTick) {
+    timeSinceTick = tFrame - Game.lastTick;
+    numTicks = (timeSinceTick / Game.tickLength) | 0;
+  }
+  updates(numTicks);
+  if (ud) {
+    Game.debug.numUpdates = numTicks;
+  }
+
+  draw(tFrame);
+  Game.lastRender = tFrame;
 }
 
 function init () {
@@ -293,22 +490,64 @@ function init () {
   canvas.onclick = function () {
     toggleFullScreen(canvas);
   };
+  window.onresize = function () {
+    updateCanvasBoundaries();
+  };
   updateCanvasBoundaries();
   document.onkeyup = ku;
   document.onkeydown = kd;
+  document.ontouchstart = ts;
   document.ontouchmove = tm;
   document.ontouchend = te;
+  Stage.spatialManager = new SpatialManager(canvas.width, canvas.height, 109);
 
-  craft = new Craft(canvas.width, canvas.height);
-  items.push(craft);
+  Game.craft = new Craft(canvas.width, canvas.height);
+  Stage.spatialManager.registerObject(Game.craft);
+  Stage.items.push(Game.craft);
 
   ctx = canvas.getContext('2d');
-  ctx.font = '16px serif';
 
-  window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(main);
 
-  fpsOut = document.getElementById('fps');
-  itemsOut = document.getElementById('items');
-  debug = document.getElementById('debug');
+}
+var hidden = 'hidden';
+
+function onVisibilityChange (evt) {
+  var evtMap = {
+        focus:true, focusin:true, pageshow:true, blur:false, focusout:false, pagehide:false
+      };
+
+  evt = evt || window.event;
+  if (evt.type in evtMap) {
+    if (evtMap[evt.type]) {
+      main(window.performance.now());
+    } else {
+      window.cancelAnimationFrame(Game.stopMain);
+    }
+  } else {
+    /*jshint validthis: true */
+    if (this[hidden]) {
+      window.cancelAnimationFrame(Game.stopMain);
+    } else {
+      main(window.performance.now());
+    }
+    /*jshint validthis: false */
+  }
+}
+// Standards:
+if (hidden in document) {
+  document.addEventListener('visibilitychange', onVisibilityChange);
+} else if ((hidden = 'mozHidden') in document) {
+  document.addEventListener('mozvisibilitychange', onVisibilityChange);
+} else if ((hidden = 'webkitHidden') in document) {
+  document.addEventListener('webkitvisibilitychange', onVisibilityChange);
+} else if ((hidden = 'msHidden') in document) {
+  document.addEventListener('msvisibilitychange', onVisibilityChange);
+} else if ('onfocusin' in document) {
+// IE 9 and lower:
+  document.onfocusin = document.onfocusout = onVisibilityChange;
+} else {
+// All others:
+  window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onVisibilityChange;
 }
 document.body.onload = init;
